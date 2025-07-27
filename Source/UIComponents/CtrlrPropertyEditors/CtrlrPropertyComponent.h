30a088ac9e9a729cf5b8ae41e4ea407cff58fc6c
Damien S <55164385+damiensellier@users.noreply.github.com>
2025-07-27 14:29:12 -0400

    Revert "Revert "5.6.64 ADDED Preferences to force B&W text for property pane text entry fields""
    This reverts commit 5e37e0fdb72998de2eda6faf8a4163bd9a00786e.

--- Source/UIComponents/CtrlrPropertyEditors/CtrlrPropertyComponent.h
+++ Source/UIComponents/CtrlrPropertyEditors/CtrlrPropertyComponent.h
@@ -605,10 +605,7 @@ class CtrlrSysExPropertyComponent  : public Component,
 class CtrlrTextPropertyComponent : public Component, public CtrlrPropertyChild
 {
 	public:
-		CtrlrTextPropertyComponent (const Value& _valueToControl,
-									const int maxNumChars,
-									const bool isMultiLine,
-									const bool isReadOnly=false);
+        CtrlrTextPropertyComponent (juce::Value valueToControl_, int maxNumChars_, bool isMultiLine_, bool isReadOnly_ = false, bool useImprovedLegibility_ = false);
 
 		~CtrlrTextPropertyComponent();
 		void refresh();
@@ -623,6 +620,7 @@ class CtrlrTextPropertyComponent : public Component, public CtrlrPropertyChild
 		ScopedPointer<Label> textEditor;
 		void createEditor (int maxNumChars, bool isMultiLine);
 		Value valueToControl;
+		bool useImprovedLegibility; // New member
 };
 
 class CtrlrTimestampProperty : public Component, public CtrlrPropertyChild
